f73d33dd4ac7f3b36e1c49bc61b8914f7ad10fb9
Vitor Lira <vitor.lira@indicium.tech>
2025-10-14 11:46:58 +0000

    Adjusting code style

--- models/marts/customers.sql
+++ models/marts/customers.sql
@@ -15,15 +15,15 @@ orders as (
 customer_orders_summary as (
 
     select
-        orders.customer_id,
+        orders.customer_id
 
-        count(distinct orders.order_id) as count_lifetime_orders,
-        count(distinct orders.order_id) > 1 as is_repeat_buyer,
-        min(orders.ordered_at) as first_ordered_at,
-        max(orders.ordered_at) as last_ordered_at,
-        sum(orders.subtotal) as lifetime_spend_pretax,
-        sum(orders.tax_paid) as lifetime_tax_paid,
-        sum(orders.order_total) as lifetime_spend
+        , count(distinct orders.order_id) as count_lifetime_orders
+        , count(distinct orders.order_id) > 1 as is_repeat_buyer
+        , min(orders.ordered_at) as first_ordered_at
+        , max(orders.ordered_at) as last_ordered_at
+        , sum(orders.subtotal) as lifetime_spend_pretax
+        , sum(orders.tax_paid) as lifetime_tax_paid
+        , sum(orders.order_total) as lifetime_spend
 
     from orders
 
