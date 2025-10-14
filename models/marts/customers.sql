23c0de6ba673c8a5818afeffa636caf0250cfced
Vitor Lira <vitor.lira@indicium.tech>
2025-10-14 02:21:18 +0000

    Adjusting model

--- models/marts/customers.sql
+++ models/marts/customers.sql
@@ -36,12 +36,12 @@ joined as (
     select
         customers.*,
 
-        customer_orders_summary.count_lifetime_orders,
-        customer_orders_summary.first_ordered_at,
-        customer_orders_summary.last_ordered_at,
-        customer_orders_summary.lifetime_spend_pretax,
-        customer_orders_summary.lifetime_tax_paid,
-        customer_orders_summary.lifetime_spend,
+        customer_orders_summary.count_lifetime_orders
+        , customer_orders_summary.first_ordered_at
+        , customer_orders_summary.last_ordered_at
+        , customer_orders_summary.lifetime_spend_pretax
+        , customer_orders_summary.lifetime_tax_paid
+        , customer_orders_summary.lifetime_spend
 
         case
             when customer_orders_summary.is_repeat_buyer then 'returning'
