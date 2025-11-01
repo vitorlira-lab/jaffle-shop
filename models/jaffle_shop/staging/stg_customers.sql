30f234b3baca77e728f0f21a649746039a0063a3
Vitor Lira <vitor.lira@indicium.tech>
2025-11-01 20:25:58 +0000

    Adjusts

--- models/jaffle_shop/staging/stg_customers.sql
+++ models/jaffle_shop/staging/stg_customers.sql
@@ -1,23 +1,15 @@
 with
+    source as (
+        select * 
+        from {{ source('ecom', 'raw_customers') }}
+    )
 
-source as (
+    , customers as (
+        select
+            id as customer_id
+            , name as customer_name
+        from source
+    )
 
-    select * from {{ source('ecom', 'raw_customers') }}
-
-),
-
-renamed as (
-
-    select
-
-        ----------  ids
-        id as customer_id,
-
-        ---------- text
-        name as customer_name
-
-    from source
-
-)
-
-select * from renamed
+select * 
+from customers
